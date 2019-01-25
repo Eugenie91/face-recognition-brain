c2f8d6b2a3b0aeb82712e64a90aad748d7c43675
Eugenie Soulange <eugeniesoulange@gmail.com>
2019-01-25 17:26:11 +0400

    deploy

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://mysterious-sierra-66159.herokuapp.com:3001/image', {
+          fetch('https://mysterious-sierra-66159.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
